e4c3b794499242e6ba44f82e74a3919818345709
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2023-12-30 16:00:32 +0000

    Added temperature functionality

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
+function updateTemperature(response) {
+  let temperature = document.querySelector("#temp");
+  let currentTemp = response.data.temperature.current;
+  temperature.innerHTML = Math.round(currentTemp);
+
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "017e2af483beec2b2e23fafdtf1f298o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(updateTemperature);
+}
+
 function replaceCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityName = document.querySelector("#city");
-  cityName.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
